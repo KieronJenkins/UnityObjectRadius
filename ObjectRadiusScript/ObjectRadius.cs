94825ce6be1930828e7fa1cbfba9ef838b4e3ea2
Kieron <17520284+KieronJenkins@users.noreply.github.com>
2023-01-25 21:43:24 +0000

    Update ObjectRadius script

--- ObjectRadiusScript/ObjectRadius.cs
+++ ObjectRadiusScript/ObjectRadius.cs
@@ -1,22 +1,59 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class ObjectRadius : MonoBehaviour
 {
-  [Range(1f, 100f)]
-  public float objectRadius = 1f;
-  public Color color = Color.red; 
+    [Range(0.0f, 10f)] [SerializeField] private float objectRadius;
+    [SerializeField] private Color gizmoColor = Color.red;
+    [SerializeField] private GizmoType gizmoType = GizmoType.WireSphere;
+    [SerializeField] private GameObject customGizmoMesh;
+    [SerializeField] private bool onlySelected = false;
 
-  private void OnEnable()
-  {
-    OnDrawGizmosSelected();
-  }
+    private bool radiusWarning = false;
 
-  private void OnDrawGizmosSelected()
-  {
-    Gizmos.color = color;
-    Gizmos.DrawWireSphere(transform.position, objectRadius);
-  }
+    private void OnDrawGizmos()
+    {
+        if (objectRadius <= 0.1f)
+            if (!radiusWarning)
+            {
+                Debug.LogWarning("The Objects Radius Has Been Set To Less Than 0.1f, Is This Correct?");
+                radiusWarning = true;
+            }
+
+        if (SelectionDrawGizmo())
+        {
+            Gizmos.color = gizmoColor;
+            switch (gizmoType)
+            {
+                case GizmoType.WireSphere:
+                    Gizmos.DrawWireSphere(transform.position, objectRadius);
+                    break;
+                case GizmoType.SolidSphere:
+                    Gizmos.DrawSphere(transform.position, objectRadius);
+                    break;
+                case GizmoType.CustomMesh:
+                    if (customGizmoMesh != null)
+                        Gizmos.DrawMesh(customGizmoMesh.GetComponent<MeshFilter>().sharedMesh, transform.position,
+                            Quaternion.identity, Vector3.one * objectRadius);
+
+                    break;
+            }
+        }
+    }
+
+    private bool SelectionDrawGizmo()
+    {
+        if (!onlySelected)
+            return true;
+        else if (Selection.activeGameObject == gameObject) return true;
+
+        return false;
+    }
+
+    private enum GizmoType
+    {
+        WireSphere,
+        SolidSphere,
+        CustomMesh
+    }
 }
